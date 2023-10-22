e80b35b1de73c2696484bbd307983270778e5f59
muhamadhaspin <muhamadhaspin@gmail.com>
2023-10-22 11:18:03 +0700

    initial project

---
+++ app/Http/Controllers/VerseController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Verse;
+use Illuminate\Http\Request;
+
+class VerseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Verse $verse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Verse $verse)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Verse $verse)
+    {
+        //
+    }
+}
