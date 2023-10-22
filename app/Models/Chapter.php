33b61128fed02b11c78623953bdb02ce5b58a233
muhamadhaspin <muhamadhaspin@gmail.com>
2023-10-22 12:56:43 +0700

    --ft : create chapter table and model

--- app/Models/Chapter.php
+++ app/Models/Chapter.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Chapter extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function verses()
+    {
+        return $this->hasMany(Verse::class);
+    }
 }
--- database/migrations/2023_10_22_041740_create_chapters_table.php
+++ database/migrations/2023_10_22_041740_create_chapters_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('chapters', function (Blueprint $table) {
             $table->id();
+
+            $table->tinyInteger('number');
+            $table->string('name');
+
+            $table->smallInteger('number_of_verses');
+            $table->enum('revelation', ['MC', 'MD'])->default('MD');
+
+            $table->tinyInteger('chronological_order');
+            $table->boolean('has_sajda');
+
             $table->timestamps();
         });
     }
