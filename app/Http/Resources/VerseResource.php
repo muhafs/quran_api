83ad903a8bbcd1e8392ac79f8ff96f3f826ad464
muhamadhaspin <muhamadhaspin@gmail.com>
2023-11-07 11:12:23 +0700

    --ft : add verse resource

---
+++ app/Http/Resources/VerseResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class VerseResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'number' => $this->number,
+            'text' => $this->text,
+            'page' => $this->page,
+            'sajda' => $this->sajda ? true : false,
+
+            'juz' => $this->juz_id,
+            'hizb' => $this->hizb_id,
+        ];
+    }
+}
